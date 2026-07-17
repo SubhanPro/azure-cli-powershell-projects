eb70dec2f39df81ba4ad813984edcdc2a0b8cca5
SubhanPro <72865360+SubhanPro@users.noreply.github.com>
2026-07-17 12:20:52 +0400

    Create Azure resource groups for DEV and PROD
    Added resource group creation for development and production environments in Azure.

--- azure-cli/deploy.sh
+++ azure-cli/deploy.sh
@@ -1,1 +1,22 @@
+#!/bin/bash
 
+set -e
+
+RESOURCE_GROUP_DEV="OCEAN-DEV" <- LOCATION="norwayeast"
+RESOURCE_GROUP_PROD="OCEAN-PROD" <- LOCATION="francecentral"
+
+
+echo "Current Azure subscription:"
+az account show --output table
+
+echo "Creating OCEAN-DEV..."
+az group create \
+  --name "$RESOURCE_GROUP_DEV" \
+  --location "$LOCATION"
+
+echo "Creating OCEAN-PROD..."
+az group create \
+  --name "$RESOURCE_GROUP_PROD" \
+  --location "$LOCATION"
+
+echo "Created resource groups:"
